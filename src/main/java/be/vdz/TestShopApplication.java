db9fe414c4c20d87cfc832528cde7bd28467b1ad
nicovdz <nicovdz26@hotmail.com>
2015-11-13 23:35:12 +0100

    initial commit

---
+++ src/main/java/be/vdz/TestShopApplication.java
@@ -0,0 +1,12 @@
+package be.vdz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestShopApplication.class, args);
+    }
+}
